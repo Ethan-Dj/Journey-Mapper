4fd5cc9ba41cf22882fe386c896fe3459512a3ff
Ethan <ethan.djanogly@gmail.com>
2023-03-28 18:46:38 +0300

    first stage login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import UploadNew from './components/UploadNew';
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
 
   return(
     <>
+    <Routes>
+      <Route path="/" element={<UploadNew/>} />
+    </Routes>
     </>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from './reducers/reducer';
+import { BrowserRouter } from 'react-router-dom';
+
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
