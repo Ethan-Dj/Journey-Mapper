0ba8479b28eb8775c3e8d815566e8705835a6e05
Ethan <ethan.djanogly@gmail.com>
2023-03-29 13:26:49 +0300

    all good now working on location coords and name

--- client/src/components/UploadNew.js
+++ client/src/components/UploadNew.js
@@ -1,9 +1,39 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const UploadNew = (props) => {
 
-    const [selectedImage, setSelectedImage] = useState(null)
+  const [selectedImage, setSelectedImage] = useState(null)
   const [changeButton, setChangeButton] = useState("Choose Photo/Video")
+  const [location, setLocation] = useState({})
+  const [locationName, setLocationName] = useState("")
+
+  const getLocation = async (position) => {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    const coords = {lat, long};
+    setLocation(coords)
+  }
+
+  useEffect(()=> {
+    window.navigator.geolocation.getCurrentPosition(getLocation);
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'a234d69e68msh0dd62b791a51637p16c7e5jsn4cf39ed5ac8d',
+        'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://trueway-geocoding.p.rapidapi.com/ReverseGeocode?location=11.830023%2C6.277828&language=en', options)
+      .then(response => response.json())
+      .then(res => console.log(res.results[0].area, res.results[0].country))
+      .catch(err => console.error(err));
+
+    console.log("running")
+    console.log(location)
+
+  },[])
 
   const display = (e) => {
     const file = e.target.files[0];
@@ -22,14 +52,15 @@ const UploadNew = (props) => {
   }
 
   const uploadImage = async (base64EncodedImage) => {
-  await fetch('http://localhost:3001/api/upload', {
+    await fetch('http://localhost:3001/api/upload', {
     method: 'POST',
     body: JSON.stringify({
       data: base64EncodedImage,
-      long: 222.222,
-      lat: 333.333,
-      time: 4444,
-      journeyname: "p",
+      locationName: "ehee",
+      long: location.long,
+      lat: location.lat,
+      imgtime: 4444,
+      journeyname: "pasta",
       userid: 1
 
     }),
@@ -40,25 +71,33 @@ const UploadNew = (props) => {
 
   return ( 
     <>
-      <h1>Hello</h1>
+      <div style = {{height:"8vh",display:"flex", flexDirection:"row", alignItems: "center", justifyContent:"space-between",borderBottom:"2px solid white" }}>
+        <button style={{width:"72px", margin:"0px 20px"}}>Cancel</button>
+        <h3 style={{fontWeight: "500"}}><u>Upload</u></h3>
+        <div style={{width:"72px", height: "30px", margin:"0px 20px"}}></div>
+      </div>
 
-      <form onSubmit = {(e)=> handleSubmit(e)}>
-        <label>
+      <div>
+      <form style={{display: "flex", flexDirection:"column", alignItems: "center"}}onSubmit = {(e)=> handleSubmit(e)}>
+        <label style={{marginTop:"50px", marginBottom:"25px"}}>
             <input type="file" name="image" onChange={(e) => display(e)} style={{ display: "none" }} />
             <span>{changeButton}</span>
         </label>
-        <input type="submit" value="post"/>
-      </form>
 
-      {!selectedImage ? null : (
-      (selectedImage.type.includes("image")) ? 
-      ( 
-        <img src={`${URL.createObjectURL(selectedImage)}`} alt="Selected" />
-      ): null)
-      (selectedImage.type.includes("video")) ? 
-      ( 
-        <video controls src={`${URL.createObjectURL(selectedImage)}`}/>
-      ): null}
+          <div style={{width:"270px", height: "270px", border:"solid 2px white"}}>
+          {selectedImage && selectedImage.type.includes("image") && 
+            <img src={`${URL.createObjectURL(selectedImage)}`} alt="Selected" />
+          }
+          {selectedImage && selectedImage.type.includes("video") && 
+            <video controls src={`${URL.createObjectURL(selectedImage)}`}/>
+          }
+        </div>
+
+        <input style={{margin:"20px"}} id="submit" type="submit" value="Add photo/video and location to journey"/>
+
+        <p style={{fontSize:"12px"}}>Only photos and videos please...</p>
+      </form>
+      </div>
     </>
     )};
 
