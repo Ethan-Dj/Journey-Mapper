6da76796f74b1b6f31d9f571dded0adcb114f71a
Ethan <ethan.djanogly@gmail.com>
2023-04-16 20:33:05 +0300

    remove cors

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const cors = require('cors')
+// const cors = require('cors')
 const dotenv = require("dotenv")
 const bodyParser = require("body-parser")
 const path = require('path')
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 dotenv.config()
 
 const content_router = require("./routes/Content.js")
